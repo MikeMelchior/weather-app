611a6f88afdfffac45d51a603bd1eefd9d0c5c4b
Mike Melchior <mike.melchior@outlook.com>
2023-03-09 17:30:32 -0500

    add function to log info for city chosen from dropdown menu which will later become function to populate the page with data about the chosen cities weather

--- src/dom-stuff.js
+++ src/dom-stuff.js
@@ -1,64 +1,65 @@
 import * as index from './index'
 
+    // function to create element and add classes in  one line
 const createClassedElement = (type, classes) => {
     let element = document.createElement(type);
     element.className = classes;
     return element;
 }
 
-
+    // main div to wrap all content
 let main = createClassedElement('div', 'main');
 document.body.append(main);
 
+    //div to wrap form and eventual dropdown menu
 let formDiv = createClassedElement('div', 'form-div');
 
+    // create form to search for city
 let form = document.createElement('form');
 let searchBar = document.createElement('input');
-    searchBar.placeholder = 'enter city';
+    searchBar.placeholder = 'Enter city';
     searchBar.maxLength = 20;
 let searchButton = document.createElement('button');
     searchButton.textContent = 'Search'
 
+    // append form to wrapper and append wrapper to main
 formDiv.appendChild(form)
 form.append(searchBar, searchButton);
 main.append(formDiv);
 
-
-let searchDropdown = createClassedElement('ul', 'search-dropdown');
-// let testOption = createClassedElement('li', 'option option-one');
-//     testOption.textContent = 'one'
-// let testOptionTwo = createClassedElement('li', 'option option-two');
-//     testOptionTwo.textContent = 'two'
-// searchDropdown.append(testOption, testOptionTwo)
-main.appendChild(searchDropdown);
-
-
-    // example dom insert
-let div = document.createElement('div');
-formDiv.after(div)
-
-
-const createNewSearchDropdown = () => {
-    document.querySelector('.search-dropdown').remove();
+    // if search dropdown exists empty it
+const emptyDropdownMenu = () => {
+    if (document.querySelector('.search-dropdown')) {
+        document.querySelector('.search-dropdown').remove();
+    };
     let element = createClassedElement('ul', 'search-dropdown');
-    formDiv.after(element);
+    formDiv.appendChild(element);
 }
 
+    // function to create each list item for search dropdown
 const createListItem = (name, state, hiddenInfo) => {
     let element = createClassedElement('li', 'search-item')
+
     let cityName = createClassedElement('p', 'city-name');
         cityName.textContent = name;
+
     let stateName = createClassedElement('p', 'state');
         stateName.textContent = state;
-    //
+
     // add lat/long info for API weather call and hide it w css;
-    //
     let hidden = createClassedElement('span', 'hidden');
-    hidden.textContent = hiddenInfo;
-    //
-    //
-    //
-    element.append(cityName, stateName)
+        hidden.textContent = hiddenInfo;
+    
+    element.append(cityName, stateName, hidden)
+
+        // before returning element, add event listener to 
+        // fill page with data if clicked
+    element.addEventListener('click', (e) => {
+        let coords = hiddenInfo.split(', ');
+        let lat = coords[0]
+        let lon = coords[1]
+        populatePage(lat, lon)
+    })
     return element
 }
 
@@ -77,12 +78,23 @@ async function populateSearchDropdown() {
         })
 }
 
-
+async function populatePage (lat, lon) {
+    let weather = await index.getWeatherByCoords(lat, lon)
+    console.log(weather);
+}
 
 searchButton.addEventListener('click', (e) => {
     e.preventDefault();
-    createNewSearchDropdown();
+    emptyDropdownMenu();
     populateSearchDropdown();
     searchBar.value = '';
 })
+
+main.addEventListener('click', (e) => {
+    if (e.target == main) {
+        emptyDropdownMenu();
+    }
+})
+
+
 
--- src/index.js
+++ src/index.js
@@ -7,63 +7,43 @@ import './styles.css';
 // http://api.openweathermap.org/data/2.5/weather?q=London&APPID=e94982487f044be7ccc7b3ba2a88955d
 //
 
-    // Example of API call for city coords :
+    // Example of API call to get city coords :
 //http://api.openweathermap.org/geo/1.0/direct?q=Oshawa&limit=5&appid=e94982487f044be7ccc7b3ba2a88955d
 //
 
     // Example of API call for weather from coords (lat/lon) :
-//https://api.openweathermap.org/data/2.5/weather?lat={43.89}&lon={-78.86}&appid=e94982487f044be7ccc7b3ba2a88955d
+//https://api.openweathermap.org/data/2.5/weather?lat=43.89&lon=-78.86&appid=e94982487f044be7ccc7b3ba2a88955d
 //
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 async function getCities(city) {
+    if (!city) {
+        city = 'toronto'
+    }
     try {
         let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=e94982487f044be7ccc7b3ba2a88955d`, 
         {mode: 'cors'});
         let data = await response.json();
-        return data
+        return data;
     } catch (e) {
         console.log(e)
     }  
 }
 
 
-async function test() {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=43.89&lon=-78.86&units=metric&appid=e94982487f044be7ccc7b3ba2a88955d`, 
+async function getWeatherByCoords(lat, lon) {
+
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}1&units=metric&appid=e94982487f044be7ccc7b3ba2a88955d`, 
         {mode: 'cors'})
     let data = await response.json();
 
     if (response.status == 200) {
-        // console.log(data);
-        //document.querySelector('.a').textContent = `${data.name} Feels like : ${data.main.feels_like} degrees`;
-        return {
-            city: data.name,
-            temp: data.main.temp,
-            feelsLike: data.main.feels_like,
-            description: data.weather[0].description,
-            windSpeed: data.wind.speed,
-        } 
+        return data;
     } else {
         throw new Error('No such data');
     }
 }
 
 
-export {getCities, test}
+export {getCities, getWeatherByCoords}
